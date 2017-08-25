9d7a0ccc267c8b278975c9875b356d70eb034632
Waleed-BS <Waleed_BS1@Guest.local>
2017-08-24 20:30:46 -0400

    feat: Add connect()
    - imported connect from react-redux in App.js
    - imported addRecipe and removeFromCalendar actions in App.js
    - changed the exported variable from App to connect()() for connecting the component and allowing it to dispatch actions
    - created mapStateToProps method which takes in the state as an argument and maps it to a usable prop
    - created mapDispatchToProps method which takes in dispatch as an argument. Useful for binding dispatch() to action creators

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { addRecipe, removeFromCalendar } from '../actions'
 
 class App extends Component {
 
-
-
-
   render() {
+    console.log('Props ', this.props)
     return (
       <div>
         Hello World
       </div>
     )
   }
-
 }
 
-export default App;
+/*
+takes in the state as an aurgment and maps it to be a usable prop
+*/
+function mapStateToProps(calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+        ? calendar[day][meal]
+        : null
+
+        return meals
+
+      }, {})
+    }))
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    //wrapping action creators inside mapDispatchToProps
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data)),
+  }
+}
+
+/*
+to dispatch an action inside this component,
+connect that component to be able to call dispatch
+*/
+export default connect(mapStateToProps, mapDispatchToProps)(App);
